51476c693b38d534f3acd7ec32995a4037d56ce1
Taco <87836366+Tacotakedown@users.noreply.github.com>
2024-06-27 04:03:20 -0600

    maybe dont push build

--- cmake-build-debug/dependencies/yaml-cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Taco/Documents/Github/InfinityInstaller/dependencies/yaml-cpp
-# Build directory: C:/Users/Taco/Documents/Github/InfinityInstaller/cmake-build-debug/dependencies/yaml-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
